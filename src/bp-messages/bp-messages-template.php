thebrandonallen/buddypress

commit 419f2b213ece387e46a79668f3969f7546895aef

Introduce Messages functions for getting and outputting the sender ID of a message in a single thread.
git-svn-id: https://buddypress.svn.wordpress.org/trunk@8674 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-messages/bp-messages-template.php
@@ -1572,6 +1572,31 @@ function bp_the_thread_message_alt_class() {
 	}
 
 /**
+ * Output the ID for message sender within a single thread
+ *
+ * @since BuddyPress (2.1.0)
+ */
+function bp_the_thread_message_sender_id() {
+	echo bp_get_the_thread_message_sender_id();
+}
+	/**
+	 * Return the ID for message sender within a single thread
+	 *
+	 * @since BuddyPress (2.1.0)
+	 *
+	 * @return string
+	 */
+	function bp_get_the_thread_message_sender_id() {
+		global $thread_template;
+
+		$user_id = ! empty( $thread_template->message->sender_id )
+			? $thread_template->message->sender_id
+			: 0;
+
+		return (int) apply_filters( 'bp_get_the_thread_message_css_class', (int) $user_id );
+	}
+
+/**
  * Output the avatar for the current message sender.
  *
  * @param array $args See {@link bp_get_the_thread_message_sender_avatar_thumb()}

commit 3517de4da463cbcef7b895e3cb79e084b93930b0

Messages: Apply correct filter name in `bp_get_the_thread_message_sender_id()`
This changeset ensures that the results of `bp_get_the_thread_message_sender_id()` are not accidentally filtered by `bp_get_the_thread_message_css_class`. 

his bug was introduced in r8674.

See #6599. For 2.3.3 (2.3 branch)

git-svn-id: https://buddypress.svn.wordpress.org/branches/2.3@10058 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

src/bp-messages/bp-messages-template.php
@@ -2287,7 +2287,7 @@ function bp_the_thread_message_sender_id() {
 		 *
 		 * @param int $user_id ID of the message sender.
 		 */
-		return (int) apply_filters( 'bp_get_the_thread_message_css_class', (int) $user_id );
+		return (int) apply_filters( 'bp_get_the_thread_message_sender_id', (int) $user_id );
 	}
 
 /**